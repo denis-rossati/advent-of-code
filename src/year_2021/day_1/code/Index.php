f407a78e6579800f4c5fadda26db778330ac4563
Denis Rossati Ramos <denis.rossatiramos@gmail.com>
2022-01-29 19:34:01 -0300

    completing part two of the first day

--- src/year_2021/day_1/code/Index.php
+++ src/year_2021/day_1/code/Index.php
@@ -4,14 +4,34 @@ declare(strict_types=1);
 
 class Index
 {
-  public static function resolution(array $sonar_sweep_list): int
+  public static function resolutionFirstPart(array $sweepList): int
   {
-    $howManyTimesIncreased = 0;
-    for($index = 0; $index < count($sonar_sweep_list); $index += 1) {
-      if ($index > 0 && ($sonar_sweep_list[$index] > $sonar_sweep_list[$index - 1])) {
-        $howManyTimesIncreased += 1;
+    $timesIncreased = 0;
+    for($index = 0; $index < count($sweepList); $index += 1) {
+      if ($index > 0 && ($sweepList[$index] > $sweepList[$index - 1])) {
+        $timesIncreased += 1;
       }
     }
-    return $howManyTimesIncreased;
+    return $timesIncreased;
+  }
+
+  public static function resolutionSecondPart(array $sweepList): int
+  {
+    $timesIncreased = 0;
+    for($index = 0; $index < count($sweepList); $index += 1) {
+      if ($index >= 3) {
+
+        $currentSweep = $sweepList[$index];
+        $secondSweep = $sweepList[$index - 1];
+        $thirdSweep = $sweepList[$index - 2];
+        $fourthSweep = $sweepList[$index - 3];
+
+        $currentSlidingWindow = $currentSweep + $secondSweep + $thirdSweep;
+        $previousSlidingWindow = $secondSweep + $thirdSweep + $fourthSweep;
+
+        if ($currentSlidingWindow > $previousSlidingWindow) $timesIncreased += 1;
+      }
+    }
+    return $timesIncreased;
   }
 }
--- src/year_2021/day_1/tests/IndexTest.php
+++ src/year_2021/day_1/tests/IndexTest.php
@@ -9,28 +9,53 @@ include_once(__DIR__ . '/../code/Index.php');
 
 final class IndexTest extends TestCase
 {
-  public function testSimpleInputReturnRigthAnswer(): void
+  public function testFirstPartSimpleInputReturnRigthAnswer(): void
   {
     
     $this->assertEquals(
       7,
-      Index::resolution(Input::useSimpleEntrie()),
+      Index::resolutionFirstPart(Input::useSimpleEntrie()),
     );
   }
 
-  public function testMediumInputReturnRightAnswer(): void
+  public function testFirstPartMediumInputReturnRightAnswer(): void
   {
     $this->assertEquals(
       16,
-      Index::resolution(Input::useMediumEntrie()),
+      Index::resolutionFirstPart(Input::useMediumEntrie()),
     );
   }
 
-  public function testCompleteInputReturnRightAnswer(): void
+  public function testFirstPartCompleteInputReturnRightAnswer(): void
   {
     $this->assertEquals(
       1301,
-      Index::resolution(Input::useCompleteEntrie()),
+      Index::resolutionFirstPart(Input::useCompleteEntrie()),
+    );
+  }
+
+  public function testSecondPartSimpleInputReturnRigthAnswer(): void
+  {
+    
+    $this->assertEquals(
+      5,
+      Index::resolutionSecondPart(Input::useSimpleEntrie()),
+    );
+  }
+
+  public function testSecondPartMediumInputReturnRightAnswer(): void
+  {
+    $this->assertEquals(
+      17,
+      Index::resolutionSecondPart(Input::useMediumEntrie()),
+    );
+  }
+
+  public function testSecondPartCompleteInputReturnRightAnswer(): void
+  {
+    $this->assertEquals(
+      1346,
+      Index::resolutionSecondPart(Input::useCompleteEntrie()),
     );
   }
 }
