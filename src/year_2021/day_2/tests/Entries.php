473fc0a481940631d55829dabcbf73b7f8c85490
Denis Rossati Ramos <denis.rossatiramos@gmail.com>
2022-01-30 18:18:14 -0300

    removing comment

--- src/year_2021/day_2/tests/Entries.php
+++ src/year_2021/day_2/tests/Entries.php
@@ -5,8 +5,8 @@ class Input
   public static function useSimpleEntrie()
   {
     return [
-      'forward 5',# h = 5 a = 0
-      'down 5',# 
+      'forward 5',
+      'down 5',
       'forward 8',
       'up 3',
       'down 8',
