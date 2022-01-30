a1856c4dbf4f980252b0ed6a77c3449c470fbb5c
Denis Rossati Ramos <denis.rossatiramos@gmail.com>
2022-01-30 18:22:13 -0300

    adding solution

--- src/year_2021/day_2/code/Index.php
+++ src/year_2021/day_2/code/Index.php
@@ -4,13 +4,42 @@ declare(strict_types=1);
 
 class Index
 {
-  public static function resolutionFirstPart(): string
+  public static function resolutionFirstPart(array $moveList): int
   {
-    return 'solution';
+    $horizontalPosition = 0;
+    $verticalPosition = 0;
+    for($index = 0; $index < count($moveList); $index += 1)
+    {
+      [$direction, $value] = explode(' ', $moveList[$index]);
+      if ($direction === 'up') {
+        $verticalPosition -= (int)$value;
+      } elseif ($direction === 'down') {
+        $verticalPosition += (int)$value;
+
+      } else {
+        $horizontalPosition += (int)$value;
+      }
+    }
+    return $horizontalPosition * $verticalPosition;
   }
 
-  public static function resolutionSecondPart(): string
+  public static function resolutionSecondPart(array $moveList): int
   {
-    return 'solution';
+    $horizontalPosition = 0;
+    $aim = 0;
+    $depth = 0;
+    for($index = 0; $index < count($moveList); $index += 1)
+    {
+      [$direction, $value] = explode(' ', $moveList[$index]);
+      if ($direction === 'up') {
+        $aim -= (int)$value;
+      } elseif ($direction === 'down') {
+        $aim += (int)$value;
+      } else {
+        $horizontalPosition += (int)$value;
+        $depth += $aim * (int)$value;
+      }
+    }
+    return $horizontalPosition * $depth;
   }
 }
