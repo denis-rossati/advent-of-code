39724e2fc1bcacd6b810b60858ffe60480430f51
Denis Rossati Ramos <denis.rossatiramos@gmail.com>
2022-01-30 18:17:52 -0300

    creating tests

--- src/year_2021/day_2/tests/IndexTest.php
+++ src/year_2021/day_2/tests/IndexTest.php
@@ -10,27 +10,50 @@ include_once(__DIR__ . '/../code/Index.php');
 final class IndexTest extends TestCase
 {
   public function testFirstPartSimpleInputReturnRigthAnswer(): void
-  {
-    
+  { 
     $this->assertEquals(
-      'ANSWER HERE',
-      Index::resolutionFirstPart(),
+      150,
+      Index::resolutionFirstPart(Input::useSimpleEntrie()),
     );
   }
 
   public function testFirstPartMediumInputReturnRightAnswer(): void
   {
     $this->assertEquals(
-      'ANSWER HERE',
-      Index::resolutionFirstPart(),
+      444,
+      Index::resolutionFirstPart(Input::useMediumEntrie()),
     );
   }
 
   public function testFirstPartCompleteInputReturnRightAnswer(): void
   {
     $this->assertEquals(
-      'ANSWER HERE',
-      Index::resolutionFirstPart(),
+      1694130,
+      Index::resolutionFirstPart(Input::useCompleteEntrie()),
+    );
+  }
+
+  public function testSecondPartSimpleInputReturnRigthAnswer(): void
+  { 
+    $this->assertEquals(
+      900,
+      Index::resolutionSecondPart(Input::useSimpleEntrie()),
+    );
+  }
+
+  public function testSecondPartMediumInputReturnRightAnswer(): void
+  {
+    $this->assertEquals(
+      5439,
+      Index::resolutionSecondPart(Input::useMediumEntrie()),
+    );
+  }
+
+  public function testSecondPartCompleteInputReturnRightAnswer(): void
+  {
+    $this->assertEquals(
+      1698850445,
+      Index::resolutionSecondPart(Input::useCompleteEntrie()),
     );
   }
 }
